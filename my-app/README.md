994cd3afe78f99036b59e15bbb98033ec2b702c7
Christina Truong <christinaqtruong@gmail.com>
2019-07-30 18:53:15 -0500

    created json file with images

--- my-app/README.md
+++ my-app/README.md
@@ -4,49 +4,4 @@
 
 This memory game was created with React and involved breaking up the application's UI into components, managing the component state, and responding to user events. The application renders different images Jerrys, Mortys and Ricks to the screen that listen for click events. The application tracks the user's score, which increments when an image is clicked for the first time. The user loses and their score is reset to 0 if they click the same image more than once. To make this game more challenging, every time an image is clicked, the images rendered to the page shuffle themselves in a random order. If the user's score is reset after an incorrect guess, the game should restart.
 
-* **This assignment must be deployed.** * Please submit both the deployed GitHub Pages link to your homework AND the link to the Github Repository!
 
-### Instructions
-
-
-7. When complete, the application should be deployed to Github Pages. See the README generated with Create React App for instructions on deploying the application to Github Pages.
-
-- - -
-
-#### Minimum Requirements
-
-* **This assignment must be deployed.** Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed. Hosting on Github Pages and adding a README.md are required for this homework. In addition, add this homework to your portfolio. More information can be found below.
-
-- - -
-
-#### Hints
-
-* Begin by building a non-functioning static version for your Clicky Game. Then work on making the game interactive.
-
-### Reminder: Submission on BCS
-
-* Please submit both the deployed GitHub Pages link to your homework AND the link to the Github Repository!
-
-- - -
-
-### Create a README.md
-
-Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
-
-* [About READMEs](https://help.github.com/articles/about-readmes/)
-
-* [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
-
-- - -
-
-### Add To Your Portfolio
-
-After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
-
-- - -
-
-#### One More Thing
-
-If you have any questions about this project or the material we have covered, please post them in the community channels in slack so that your fellow developers can help you! If you're still having trouble, you can come to office hours for assistance from your instructor and TAs.
-
-**Good Luck!**
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PictureCard from "./components/PictureCard";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    clickedArray: [],
+    topScore: 0,
+    score: 0,
+    message: "",
+    shakeit: "false"
+  };
+  clickPicture = id => {
+    // Arrange the pictures in a random manner
+    const shuffledArray = this.shuffleArray(cards);
+    this.setState({cards: shuffledArray});
+    // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
+    if (this.state.clickedArray.includes(id)) {
+      this.setState({ score: 0, clickedArray: [], message: "Congratulations. You're dumber than Jerry.", shakeit: "true"});
+    }
+    else {
+      this.setState({
+        clickedArray: this.state.clickedArray.concat([id]),
+        score: this.state.score + 1,
+        message: "Woah there buddy. Keep going and you might beat Morty's high score.",
+        shakeit: "false"
+      });
+    }
+    // set topscore = score if score>topscore.
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+    // shake the wrapper if shakeit is set to true
+  }
+  shuffleArray = (picturesArray) => {
+      for (let i = picturesArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+      }
+      return picturesArray;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Clicky Clicky 1000 TIMES CLICKY!!!</h1>
+        </header>
+        <h3 className="App-intro">
+          <strong>To play, just click on each unique image once. Click the same image twice and you lose. It's so simple even a Morty could play.</strong> 
+          <p className = "score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+          <p className="message"><strong>{this.state.message}</strong></p>
+        </h3>
+        <Wrapper
+        shakeWrapper = {this.state.shakeit}
+        pictures=
+          {this.state.cards.map(picture => (
+            <PictureCard
+              clickPicture={this.clickPicture}
+              id={picture.id}
+              key={picture.id} // to get rid of unique key prop warning
+              name={picture.name}
+              image={picture.image}
+            />
+          ))}
+        />
+        <footer className="footer">
+      <div className="container">
+      
+      </div>
+    </footer> 
+      </div>
+    );
+  }
 }
 
 export default App;
