fc02acffad07f40de910b2ae043841cc51d7a248
Christina Truong <christinaqtruong@gmail.com>
2019-07-30 19:34:49 -0500

    Updates

---
+++ precache-manifest.e082edac5ed229d466a9af5a4f855b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280b903a58b22b79d24d07e9f22c8532",
+    "url": "/RickandMortyClickyGame/index.html"
+  },
+  {
+    "revision": "1f9f5a14dacb8a646546",
+    "url": "/RickandMortyClickyGame/static/css/main.954524cb.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/RickandMortyClickyGame/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "1f9f5a14dacb8a646546",
+    "url": "/RickandMortyClickyGame/static/js/main.ed9dea56.chunk.js"
+  },
+  {
+    "revision": "48e15aa40a83648b3b66",
+    "url": "/RickandMortyClickyGame/static/js/runtime~main.707b8f3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RickandMortyClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
